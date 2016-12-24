6c457272f4e86441108ccd7d8ad0a0a6a32f163f
msciborski <michal.sciborski@student.put.poznan.pl>
2016-12-24 19:55:03 +0100

    Implement BSTNode, which representing single node of tree.

---
+++ BST/BST/BSTNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST {
+    public class BSTNode {
+        public int Value { get; set; }
+        public BSTNode LeftNode { get; set; }
+        public BSTNode RightNode { get; set; }
+
+        public BSTNode(int value){
+            Value = value;
+            LeftNode = null;
+            RightNode = null;
+        }
+    }
+}
