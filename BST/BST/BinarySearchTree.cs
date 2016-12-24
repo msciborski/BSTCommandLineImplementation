6b00e4222cd85facecd6992a22b3c26d8db5ac06
msciborski <michal.sciborski@student.put.poznan.pl>
2016-12-24 20:06:49 +0100

    Adding BinnarySearchTree class, which providing methods(addtoBST, search, etc.)

---
+++ BST/BST/BinarySearchTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST {
+    public class BinarySearchTree{
+        public BSTNode Root { get; set; }
+
+        public BinarySearchTree(BSTNode root){
+            Root = root;
+        }
+
+
+    }
+}
