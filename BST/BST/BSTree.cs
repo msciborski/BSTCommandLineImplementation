1406209fd4f9baf0f345d549e94d481a2f105ef6
msciborski <michal.sciborski@student.put.poznan.pl>
2016-12-25 16:30:53 +0100

    Adding Insert Method in BSTree class

---
+++ BST/BST/BSTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST {
+    public class BSTree {
+        public BSTNode Root { get; set; }
+
+        private BSTree() { }
+
+        /// <summary>
+        /// Method- add element to the binary tree, if root == null, create root, else look at the nodes and try to add node
+        /// </summary>
+        /// <param name="value"></param>
+        /// 
+
+        public void Insert(int value){
+            if (Root != null){
+                Root.Value = value;
+            }
+            else{
+                Insert(Root,value);
+            }
+        }
+
+        /// <summary>
+        /// Overloaded method. Adding value to the tree.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="value"></param>
+        /// 
+
+        private void Insert(BSTNode root, int value){
+            if (value == root.Value){
+                return;
+            }
+            if (root.Value > value){
+                if (root.LeftNode == null){
+                    root.LeftNode = new BSTNode(value);
+                }
+                else{
+                    Insert(root.LeftNode, value);
+                }
+            }
+            else{
+                if (root.RightNode == null){
+                    root.RightNode = new BSTNode(value);
+                }
+                else{
+                    Insert(root.RightNode,value);
+                }
+            }
+        }
+
+
+    }
+}
